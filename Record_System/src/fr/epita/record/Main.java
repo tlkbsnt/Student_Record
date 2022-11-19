dfd8a6b64f79aad706437c30eccc0630df1bd865
tlkbsnt <tlkbsntnpl@gmail.com>
2022-11-19 20:04:52 +0100

    This is java system

--- Record_System/src/fr/epita/record/Main.java
+++ Record_System/src/fr/epita/record/Main.java
@@ -3,7 +3,7 @@ package fr.epita.record;
 public class Main {
 
     public static void main(String[] args) {
-
+// This is main class
         Student student = new Student();
         student.working();
     }
